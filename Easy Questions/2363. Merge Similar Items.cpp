2f3470afc52c964253b28fe94929bb86a89055ea
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-09-20 23:35:14 +0530

    Create 2363. Merge Similar Items.cpp

---
+++ Easy Questions/2363. Merge Similar Items.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int,int> m;
+
+        for(int i=0;i<items1.size();i++)
+        {
+            m[items1[i][0]] = items1[i][1];
+        }
+        for(int i=0;i<items2.size();i++)
+        {
+            m[items2[i][0]] += items2[i][1];
+        }
+
+        vector<vector<int>> ans;
+         for(auto x: m)
+         {
+             ans.push_back({x.first, x.second});
+         }
+
+return ans;
+    }
+};
