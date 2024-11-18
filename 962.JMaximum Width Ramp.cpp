907ce01441bc8a1a66acd8266d1eda460e46c42e
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2024-11-18 23:25:49 +0530

    Update 962.JMaximum Width Ramp.cpp

--- 962.JMaximum Width Ramp.cpp
+++ 962.JMaximum Width Ramp.cpp
@@ -13,7 +13,7 @@ public:
         
         int maxWidth = 0;
         
-        // Step 2: Traverse from the end and find maximum width ramp
+        
         for (int j = n - 1; j >= 0; --j) {
             while (!s.empty() && nums[s.top()] <= nums[j]) {
                 maxWidth = max(maxWidth, j - s.top());
