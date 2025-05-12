ced6c9e12970a876dd31154f227c3a0e2f0a5a8f
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2025-05-12 22:35:06 +0530

    Update 1208. Get Equal Substrings Within Budget.cpp

--- Medium Question/1208. Get Equal Substrings Within Budget.cpp
+++ Medium Question/1208. Get Equal Substrings Within Budget.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
         int n = s.size();
+        
         int start = 0;
         int current_cost = 0;
         int max_length = 0;
